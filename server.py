cbb3b83f312a7e1888a30adb63d3a33bb7730f93
Robert Godlewski <robtechg5@gmail.com>
2022-02-03 14:07:55 -0800

    Made a basic counter using flask.

--- server.py
+++ server.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session
 
 
 app = Flask(__name__)
 
+app.secret_key = 'hello'
+
 @app.route('/')
-def hello(): return render_template("index.html")
+def counter(): 
+    if 'count' not in session: 
+        print('\n', str(session))
+        print('count doesn\'t exist.\nNow adding it in?')
+        session['count'] = 0
+        print(f'session details: {str(session)}')
+        print(f"count in session: {str(session['count'])}")
+    else: 
+        session['count'] += 1
+        print(f"\nCount is now: {str(session['count'])}")
+    return render_template("index.html")
+
+@app.route('/destroy_session')
+def destroy_session(): 
+    session.clear()
+    print('\nRestarted the count.')
+    return redirect('/')
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+if __name__ == "__main__": app.run(debug=True, port=8000)
